b3a2e4e6c1e29cb48ff0380db17b97ccb0a0127f
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-16 16:23:22 +0100

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,22 +11,23 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-  
-else if (b > a && b > c)
-{
-largest = b;
-}
-  
-else 
-{
-largest = c;
-}
-
-return (largest);
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
