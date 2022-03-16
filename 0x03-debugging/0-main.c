d67c8d9b02cb9fb488afe6659e867561d2213f21
Simplyvoda <efem.vodina@gmail.com>
2022-03-16 15:24:15 +0100

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: Always Success (0)
+ *
+ * */
+int main(void)
+{
+	int i;
+	i=50;
+	positive_or_negative(i);
+
+	return (0);
+}
