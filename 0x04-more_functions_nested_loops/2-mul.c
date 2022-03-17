d9382232fff45954fab57972093d23283e3fd1f4
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-17 12:19:53 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b);
+{
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
+}
