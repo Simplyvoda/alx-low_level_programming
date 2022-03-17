9e389f0d743490d09e6393dbd0bf97340786b2a3
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-17 08:40:51 +0100

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
-#include "main.h"
 /**
-* main - prints the first  98 fibonacci numbers
-*
-* Return: 0
-*/
-
-void printFibonacciNumbers(int n)
+  * main - print the first 98 fibonacci numbers.
+  * Return: Nothing.
+  */
+int main(void)
 {
-	int f1 = 0, f2 = 1, i;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	if (n < 1)
-		return;
-	printf("%d, ", f1);
-	for (i = 1; i < n; i++) {
-		printf("%d, ", f2);
-		int next = f1 + f2;
-		f1 = f2;
-		f2 = next;
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
-}
-
-int main()
-{
-	printFibonacciNumbers(98);
-	return 0;
-}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
