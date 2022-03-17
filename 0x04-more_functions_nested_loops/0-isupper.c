4e6be5eadd10864a20303a79f7a3dd1505f76d26
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-17 11:15:43 +0100

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,13 +7,13 @@
  * Return: Always 0
  *
  * /
- int main(void)
- {
-     char c;
+int main(void)
+{
+    char c;
 
-         c = 'A';
-	     printf("%c: %d\n", c, _isupper(c));
-	         c = 'a';
-		     printf("%c: %d\n", c, _isupper(c));
-		         return (0);
-			 }
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
