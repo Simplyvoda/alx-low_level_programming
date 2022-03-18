03b31585014362793af962298c43c3481962687d
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-18 20:48:05 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 without 2 and 4
+ *
+ * ch - character
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+    char ch = '0'
+    for (ch = '0'; ch <= '9'; ch++)
+    {
+        if (ch == 2 or ch == 4)
+        continue;
+
+        else
+        putchar(ch);
+    }
+    putchar('\n');
+}
