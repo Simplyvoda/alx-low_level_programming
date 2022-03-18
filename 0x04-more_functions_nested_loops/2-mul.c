d6b42cb7c43d0548952b9a0731e8132c5169e21d
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-18 16:09:59 +0100

    commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * @a - first integer
+ * @b - second integer
+ * main - multiplies 2 integers
+ * Return: Always 0
  */
+
 int mul(int a, int b);
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
+    printf(a*b);
     return (0);
 }
