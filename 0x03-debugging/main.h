adc8846db3ce30f40cb338d915b2469363bbef18
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-18 14:01:54 +0100

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-int main(void)
-void positive_or_negative(int i)
-int largest_number(int a, int b, int c)
-void print_remaining_days(int month, int day, int year)
-int convert_day(int month, int day)
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
