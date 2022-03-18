ad7d1f180552092ec358255e8f8ebb12fd179feb
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-18 16:24:34 +0100

    commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * @brief this programme is by VODINA EFEM
- * mul - multiplies two integers
+ * @mul - multiplies two integers
+ *
  * @param a is the first integer
  * @param b is the second integer
  * @return int
