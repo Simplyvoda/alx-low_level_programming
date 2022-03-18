8c8865a9c432125b3c0bd0d6d2f3cc7ac4062d30
Vodina Efem <efem.vodina@gmail.com>
2022-03-18 19:02:28 +0000

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * ch - character
- * 
+ *
  * print_numbers - prints numbers 0 to 9
- * 
+ *
  * Return: 0
  */
 
 void print_numbers(void)
 {
-    char ch = '0';
-    for (ch = '0'; ch <= '9'; ch++)
-        putchar(ch);
+	char ch = '0';
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
 
-    return (0);
+	return (0);
 }
