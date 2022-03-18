43fb45f341ab3e46a98cd20ac4ca12a473d21af3
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-18 15:56:35 +0100

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,13 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-
-
-#endif /* _SAMPLE_HEADER_H_ */
+#endif /* _MAIN_H_ */
