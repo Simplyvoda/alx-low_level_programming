a75b31a29dcc569f847a82c1d7342a658c3b1507
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-03-21 20:18:59 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Directs a pointer to an int and changes
+ * and changes the value to 98.
+ *
+ * @n: The variable pointing to the integer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
