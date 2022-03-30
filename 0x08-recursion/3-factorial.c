dbd11b23e77f18c4a03d792af06e15c8f960e2ee
Vodina Efem <efem.vodina@gmail.com>
2022-03-30 16:20:43 +0000

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * int factorial - returns factorial of given number
+ *
+ * @n: number given
+ *
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
