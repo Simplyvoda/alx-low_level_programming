3fdc4188d126186615a2d0ae56af00862f497085
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-04-01 13:53:56 +0100

    commit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int main(int argc, char *argv[]);
-void print_string(char *str);
-int main(int argc __attribute__((unused)), char *argv[]);
 int _putchar(char c);
+void print_string(char *str);
+void print_number(int num);
+int main(int argc __attribute__((unused)), char *argv[]);
+
 
 #endif
