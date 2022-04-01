0bddfc054acc998a8c035eb244ce6511cbdb9f9d
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-04-01 12:55:46 +0100

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -19,3 +19,22 @@ int main(int argc __attribute__((unused)), char *argv[])
 
 	return (0);
 }
+
+/**
+ * print_string - Prints all char of a string
+ *
+ * @str: Pointer to string
+ *
+ * Return: void
+ */
+
+void print_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+}
