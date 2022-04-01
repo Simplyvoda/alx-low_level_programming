0b8e4813cca492cc3e0ea7fc6eab7eb9ff165877
Vodina Efem <efem.vodina@gmail.com>
2022-04-01 10:46:07 +0000

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints the programs name
+ *
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The programs name is %s\n", argv[0]);
+	return 0;
+}
