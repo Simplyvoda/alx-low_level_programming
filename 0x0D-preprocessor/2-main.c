ac6834e2bbb6eab30f88b125b615b6f0636b73c6
Simplyvoda <efem.vodina@gmail.com>
2022-04-11 16:23:28 +0100

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
+
 int main()
 {
 	printf("The file name of this program is %s\n", __FILE__);
