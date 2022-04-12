2921f2ab4e1a84ce06de17ca466b2c956085ac7a
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-04-12 21:00:32 +0100

    commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,12 +15,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return (NULL);
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-		return d;
 	}
 }
