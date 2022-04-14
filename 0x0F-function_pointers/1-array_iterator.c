7ccae9d9cc6bb0be98aa5748f649768243abb800
Simplyvoda <99282239+Simplyvoda@users.noreply.github.com>
2022-04-14 11:46:39 +0100

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,7 +18,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL || size = 0 || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
