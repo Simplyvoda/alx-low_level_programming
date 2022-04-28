11291587170cc118bb7d8f5d13154a6ebb14004b
Simplyvoda <efem.vodina@gmail.com>
2022-04-28 20:33:18 +0100

    create print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: parameter
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i; count = 0;
+	unsigned long int pre_value;
+
+	for (i = 63; i >= 0; i--)
+	{
+		pre_value = n >> i;
+
+		if (pre_value & 1)
+		{
+			_putchar('1');
+			count ++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
