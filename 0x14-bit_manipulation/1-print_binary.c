3bea3fd433c81a04d85cb3770a07279c4fd2144a
Simplyvoda <efem.vodina@gmail.com>
2022-04-29 10:32:33 +0100

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,21 +10,21 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int pre_value;
+        int i, count = 0;
+        unsigned long int pre_value;
 
-	for (i = 63; i >= 0; i--)
-	{
-		pre_value = n >> i;
+        for (i = 63; i >= 0; i--)
+        {
+                pre_value = n >> i;
 
-		if (pre_value & 1)
-		{
-			_putchar('1');
-			count ++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+                if (pre_value & 1)
+                {
+                        _putchar('1');
+                        count ++;
+                }
+                else if (count)
+                        _putchar('0');
+        }
+        if (!count)
+                _putchar('0');
 }
