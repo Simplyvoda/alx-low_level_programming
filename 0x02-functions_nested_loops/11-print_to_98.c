a064740e2c8205586b6913060d950f5ee40553c4
Vodina Victor Efem <efem.vodina@gmail.com>
2022-05-11 20:57:11 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,27 +8,21 @@
  * Return: no return.
  */
 
-#include <stdio.h>
-
-void print_to_98(int n);
 void print_to_98(int n)
-
 {
-	int i;
-
-	for (i = n; i < 98; i++)
+	if (n > 98)
 	{
-		printf("%d, ", i);
-	}
-	if (i == 98)
-	{
-		printf("%d\n", i);
-	}
-	else
-	{
-		for (i = n; i > 98; i--)
+		for (; n > 98; n--)
 		{
-			printf("%d, ", i);
+			printf("%d, ", n);
 		}
 	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
