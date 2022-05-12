eff1c90567a6566a135df301684b0b630cec4062
Vodina Efem <efem.vodina@gmail.com>
2022-05-12 02:43:59 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * more_numbers - Print 10x the numbers 0-14
+ * more_numbers - prints 10 times the numbers from 0-14
  *
- * Return: 10x the numbers 0-14
+ * Return: void
  */
 
 void more_numbers(void)
 {
-    int i;
-    int n;
-    
-    for (i = 0; i < 11; i++)
-    {
-        for (n = 0; n < 15; n++)
-        {
-            if (n > 9)
-            {
-                _putchar((b / 10) + '0');
-            }
-            
-            _putchar((b % 10) + '0');
-        }
-        
-        _putchar('\n');
-    
-    }
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (n = 0; n < 15; n++)
+	{
+	if (n > 9)
+	{
+	_putchar((n / 10) + '0');
+	}
+
+	_putchar((n % 10) + '0');
+	}
+
+	_putchar('\n');
+
+	}
 }
