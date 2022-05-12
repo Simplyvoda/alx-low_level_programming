ff0e928b10fa450d90bfcaffd7a907c2a8e6970a
Simplyvoda <efem.vodina@gmail.com>
2022-05-12 13:21:45 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints line to terminal
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	
+	_putchar('\n');
+}
+
