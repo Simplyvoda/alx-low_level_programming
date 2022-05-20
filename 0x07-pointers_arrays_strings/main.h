69d4a32b676de1fa27eb29a2b0b517ec84b186e0
Vodina Victor Efem <efem.vodina@gmail.com>
2022-05-20 10:12:14 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
