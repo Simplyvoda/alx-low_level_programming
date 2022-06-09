58f4963107442a80f6d87bba4db8ca2e3bd42c07
Vodina  Efem <efem.vodina@gmail.com>
2022-06-09 15:23:12 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,6 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		current_node = current_node->next;
 	
 	new_node = malloc(sizeof(dlistint_t));
+	
 	if (new_node == NULL)
 	{
 		free(new_node);
