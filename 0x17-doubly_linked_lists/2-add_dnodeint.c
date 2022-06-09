75289776ca4ebe3174d85cec1b6a5f4b1650c449
Vodina  Efem <efem.vodina@gmail.com>
2022-06-09 15:21:29 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = *head;
-	new_node->prev = NULL;
+new_node->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
