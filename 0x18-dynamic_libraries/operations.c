1f4ce7a9e52da2fd377d89a3bf418e0886b40e6e
Simplyvoda <efem.vodina@gmail.com>
2022-06-13 20:19:41 +0100

    commit

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * add - adds two integers
+ * @a - first integer
+ * @b - second integer
+ */
+int add(a, b)
+{
+	return (a + b);
+}
+
+/*
+ * sub - function to find differnece of two integers
+ * @a - first integer
+ * @b - second integer
+ */
+
+int sub(a, b)
+{
+        return (a - b);
+}
+
+/*
+ * mul - multiplies two integers
+ * @a - first integer
+ * @b - second integer
+ */
+
+int mul(a, b)
+{
+        return (a * b);
+}
+
+/*
+ * div - function that divides two integers
+ * @a - first integer
+ * @b - second integer
+ */
+
+int div(a, b)
+{
+        return (a / b);
+}
+
+/*
+ * mod - modulus of two integers
+ * @a - first integer
+ * @b - second integer
+ */
+
+int mod(a, b)
+{
+        return (a % b);
+}
