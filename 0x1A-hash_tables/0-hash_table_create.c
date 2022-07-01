e9ed0ef3012f20eaa28f8015b9d84f38ed278dbf
Vodina  Efem <efem.vodina@gmail.com>
2022-07-01 21:18:00 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_create - function to create hash tables
  * @size: size of array
@@ -8,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table;
-	int i;
+	unsigned long int i;
 
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
