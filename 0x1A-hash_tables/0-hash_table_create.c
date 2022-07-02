b55b059f140941b93833310689a976f0de39eee3
Vodina  Efem <efem.vodina@gmail.com>
2022-07-02 17:06:26 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (h_table == NULL)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = malloc(size * sizeof(hash_node_t *));//why node for array?
+	h_table->array = malloc(size * sizeof(hash_node_t *));
 	if (h_table->array == NULL)
 	{
 		free(h_table);
