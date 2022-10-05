e05f3dff3a90d89d3be64abb6d1273fe5cc594e1
Vodina Efem <efem.vodina@gmail.com>
2022-10-05 15:02:29 +0000

    fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,68 +1,52 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include "strlen.c"
-
-list_t *createNewNode(const char *str);
 
 /**
- * add_node_end - dds a new node at the end of a list_t list
- * @head: douple pointer to the head of the linked list
- * @str: pointer to string to be assigned to the added node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
+ * add_node_end - adds a new node at the end of list_t list
+ * @head: double pointer to the head of the linked list
+ * @str: pointer to string
+ * Return: SUCCESS, NULL or FAILURE
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current_node = *head;
+	list_t *last;
+	list_t *new;
+	char *copy;
+	int i;
 
-	if (current_node)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		while (current_node)
-		{
-			if (current_node->next)
-			{
-				current_node = current_node->next;
-			}
-			else
-			{
-				current_node->next = createNewNode(str);
-				return (current_node->next);
-			}
-		}
+		return (NULL);
+	}
+
+	copy = strdup(str);
+	if (copy == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; str[i];)
+		i++;
+
+	new->str = copy;
+	new->len = i;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
 	}
 	else
 	{
-		*head = createNewNode(str);
+		last = *head;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new;
 	}
-
 	return (*head);
-}
-
-/**
- * createNewNode - create a new list_t list node
- * @str: pointer to string to be assigned to the created node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
- */
-
-list_t *createNewNode(const char *str)
-{
-	list_t *new_node_ptr = malloc(sizeof(list_t));
-
-	if (!new_node_ptr)
-		return (NULL);
-
-	new_node_ptr->str = strdup(str);
-
-	if (!(new_node_ptr->str))
-	{
-		free(new_node_ptr);
-		return (NULL);
-	}
-
-	new_node_ptr->len = _strLen(new_node_ptr->str);
-	new_node_ptr->next = NULL;
-
-	return (new_node_ptr);
 }
